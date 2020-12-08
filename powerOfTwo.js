b416ac72f4b1cdd2886b7ead866d1776bb996ac7
dannyflatiron <dannyflatiron@gmail.com>
2020-12-08 00:44:14 -0500

    poewrOfTwo algo

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -1,2 +1,19 @@
 // determine whether an input number is a power of two
+// divide number and future division results by two, until 1 
+// is reached and check if remainder is always 0
 
+function isPowerOfTwo(number) {
+
+  if (number < 1) {
+    return false
+  }
+
+  let dividedNumber = number
+  while (dividedNumber !== 1) {
+    if (dividedNumber % 2 !== 0) {
+      return false
+    }
+    dividedNumber = dividedNumber / 2
+  }
+  return true
+}
