ebc133c5b7f39d48b97a7f7d89afc4df8c8b40d7
dannyflatiron <dannyflatiron@gmail.com>
2020-12-11 09:20:21 -0500

    removing leaf nodes

--- data structures/binarySearchTree.js
+++ data structures/binarySearchTree.js
@@ -3,6 +3,7 @@ class Node {
    this.value = value 
    this.left = null
    this.right = null
+   this.parent = null
   }
 
   add(value) {
@@ -17,6 +18,7 @@ class Node {
         return
       }
       const newNode = new Node(value)
+      newNode.parent = this
       this.right = newNode
       return
     }
@@ -27,9 +29,36 @@ class Node {
         return
       }
       const newNode = new Node(value)
+      newNode.parent = this
       this.left = newNode
       return
     }
+  }
+
+  remove(value) {
+    const identifiedNode = this.find(value)
+
+    if (!identifiedNode) {
+      throw new Error('Could not fnd node with that value')
+    }
+
+    if (!identifiedNode.left && !identifiedNode.right) {
+      const identifiedParent = identifiedNode.parent
+      identifiedParent.removeChild(identifiedNode)
+    }
+  }
+
+  removeChild(node) {
+    if (this.left && this.left === node) {
+      this.left = null
+      return
+    }
+
+    if (this.right && this.right === node) {
+      this.right = null
+      return
+    }
+
   }
 
   find() {
