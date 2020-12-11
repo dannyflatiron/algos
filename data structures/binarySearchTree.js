f9ff5c8a4d2903ec140eac9e70f8a186750bfeb4
dannyflatiron <dannyflatiron@gmail.com>
2020-12-11 08:54:29 -0500

    binarySearchtree

---
+++ data structures/binarySearchTree.js
@@ -0,0 +1,45 @@
+class Node {
+  constructor(value){
+   this.value = value 
+   this.left = null
+   this.right = null
+  }
+
+  add(value) {
+    if (this.value === null) {
+      this.value = value
+      return
+    }
+
+    if (this.value < value) {
+      const newNode = new Node(value)
+      this.right = newNode
+      return
+    }
+
+    if (this.value > value) {
+      this.left = newNode
+      return
+    }
+  }
+
+
+}
+
+class Tree {
+  constructor() {
+    this.root = new Node(null)
+  }
+
+  add(value) {
+    this.root.add(value)
+  }
+
+  remove(value) {
+    this.root.remove(value)
+  }
+
+  find(value) {
+
+  }
+}
