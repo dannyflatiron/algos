be9d480575b12db6b5047b4d0b2bf78c89b26661
dannyflatiron <dannyflatiron@gmail.com>
2021-01-24 02:32:44 -0500

    solve fib algo

--- fibonacci.js
+++ fibonacci.js
@@ -4,6 +4,10 @@ function getNthFib(n) {
   let counter = 3
 
   while (counter <= n) {
-
+    let nextFib = lastTwo[0] + lastTwo[1]
+    lastTwo[0] = lastTwo[1]
+    lastTwo[1] = nextFib
+    counter +=1
   }
+  return n > 1 ? lastTwo[1] : lastTwo[0]
 }
