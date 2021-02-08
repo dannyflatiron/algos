50b982f52d308e2f0670880e027b840522390fea
dannyflatiron <dannyflatiron@gmail.com>
2021-02-07 23:46:47 -0500

    finish breadth first search

--- breadthFirstSearch.js
+++ breadthFirstSearch.js
@@ -5,6 +5,16 @@ class Node {
   }
 
   breadthFirstSearch(array) {
-    
+    let queue = [this]
+		let currentNode
+		
+		while (queue.length > 0) {
+			currentNode = queue.shift()
+			array.push(currentNode.name)
+			for (let i = 0; i < currentNode.children.length; i++) {
+				queue.push(currentNode.children[i])
+			}
+		}
+		return array
   }
 }
